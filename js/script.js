13745d6175d80e5103a386caadd7c4405708a557
bjayanta <bjayanta.me@gmail.com>
2017-01-07 16:37:47 +0600

    project initiate

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var findMeButton = $('.find-me');
+
+// Check if the browser has support for the Geolocation API
+if (!navigator.geolocation) {
+
+  findMeButton.addClass("disabled");
+  $('.no-browser-support').addClass("visible");
+
+} else {
+
+  findMeButton.on('click', function(e) {
+
+    e.preventDefault();
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+      // Get the coordinates of the current possition.
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+
+      $('.latitude').text(lat.toFixed(3));
+      $('.longitude').text(lng.toFixed(3));
+      $('.coordinates').addClass('visible');
+
+      // Create a new map and place a marker at the device location.
+      var map = new GMaps({
+        el: '#map',
+        lat: lat,
+        lng: lng
+      });
+
+      map.addMarker({
+        lat: lat,
+        lng: lng
+      });
+
+    });
+
+  });
+
+}
